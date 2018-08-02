4d86c36e0eb8e8ac95ed5b4ecfcbd329e9cac385
Moyeong Lee <2000prcs@gmail.com>
2018-08-02 14:23:36 -0700

    Initial Setup

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
+
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        Index Page
+      </div>
+    );
+  }
+}
