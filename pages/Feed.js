4e046e0a867e5c66b810a1d1b956002d643971d9
Moyeong Lee <2000prcs@gmail.com>
2018-08-04 13:01:24 -0700

    Add default image for image placeholder

--- pages/Feed.js
+++ pages/Feed.js
@@ -1,32 +1,18 @@
-import Link from 'next/link';
-import Head from 'next/head';
 import React from 'react';
-import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { arrayOf, object } from 'prop-types';
+import { ListGroup } from 'reactstrap';
 import FeedItem from './FeedItem';
 
-export default class Feed extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-  }
+const Feed = ({ newsData }) => {
+  return (
+    <ListGroup>
+      {newsData.map(news => <FeedItem key={news.id} news={news} />)}
+    </ListGroup>
+  );
+};
 
-  render() {
+Feed.propTypes = {
+  newsData: arrayOf(object.isRequired).isRequired,
+};
 
-    const { newsData } = this.props;
-    console.log(newsData);
-
-    return (
-      <ListGroup>
-        { newsData.map(news => <FeedItem news={news} />) }
-        <ListGroupItem>
-          <ListGroupItemHeading>List group item heading</ListGroupItemHeading>
-          <ListGroupItemText>
-          Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
-          </ListGroupItemText>
-        </ListGroupItem>
-      </ListGroup>
-    );
-  }
-}
+export default Feed;
--- pages/FeedItem.js
+++ pages/FeedItem.js
@@ -1,28 +1,42 @@
-import Link from 'next/link';
-import Head from 'next/head';
 import React from 'react';
-import { ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { object } from 'prop-types';
+import {
+  ListGroupItem,
+  Media,
+  Badge,
+} from 'reactstrap';
 
-export default class Feed extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-  }
+const FeedItem = ({ news }) => {
 
-  render() {
-    const { news } = this.props;
+  const imgStyle = {
+    height: '160px',
+    width: '160px',
+    margin: '10px',
+  };
 
-    return (
-      <ListGroupItem>
-        <ListGroupItemHeading>
+  const textStyle = {
+    fontStyle: 'italic',
+  };
+
+  return (
+    <ListGroupItem>
+      <Media>
+        <Media left href={news.url} target="_blank">
+          <Media style={imgStyle} object src={news.image_url} onError={(e) => {e.target.src="../static/image.jpg"}} alt="news-image" />
+        </Media>
+        <Media body>
+          <Media heading>
           {news.title}
-        </ListGroupItemHeading>
-        <ListGroupItemText>
+        </Media>
           {news.description}
-        </ListGroupItemText>
-      </ListGroupItem>
-    );
-  }
-}
+          <br />
+          <br />
+          <span style={textStyle} >Source: <a href={news.site.url} target="_blank">{news.site.name}</a></span>
+        </Media>
+          <Badge color="secondary">News Score: {Math.ceil(news.score)}</Badge>
+      </Media>
+    </ListGroupItem>
+  );
+};
+
+export default FeedItem;
